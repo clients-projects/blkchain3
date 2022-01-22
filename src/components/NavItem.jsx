b384f82eb1fe27612d60703028941a0d3670bbc2
Igboanugwo Collins <munisco12@gmail.com>
2022-01-22 21:06:32 +0100

    working in header

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { AiFillCaretDown } from 'react-icons/ai'
+import { BsChevronDown } from 'react-icons/bs'
 
 const NavItem = ({ name, link_path }) => {   
 
     return (
         <li className=' py-1 lg:py-3 px-4 lg:px-4  lg:self-center text-base flex '>
-            <a href={link_path}>
-                {name}{' '}
-                
-            </a>
+            <a href={link_path}>{name} </a>
             {name === 'Products' ? (
-
-                    <AiFillCaretDown className='self-center text-gray-300' style={{marginLeft: '.2rem', marginTop: '-3px'}} />
-            ) : ''}
+                <BsChevronDown
+                    className='self-center text-gray-300'
+                    style={{ marginLeft: '.2rem', marginTop: '-3px' }}
+                />
+            ) : (
+                ''
+            )}
         </li>
     )
 }
